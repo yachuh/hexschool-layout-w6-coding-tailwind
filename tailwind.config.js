9f047df7b0e1a21feeec7f7a67f1fa7588c1164a
yachuh <yachuh@gmail.com>
2023-04-18 23:49:37 +0800

    Setup Tailwind customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: ".75rem",
+    },
+    extend: {
+      fontFamily: {
+        azeret: ['"Azeret Mono"'],
+      },
+      colors: {
+        transparent: "transparent",
+        qreen: "#62DB54", // 綠色
+        black: {
+          primary: "#1C1C1C", // 黑色, 主要背景
+          secondary: "#2D2D2D", //深灰色, 次要背景
+        },
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
