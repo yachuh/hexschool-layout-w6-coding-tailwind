d3f3b3ae506aefb81c504c82e624c1d31432f3f9
yachuh <yachuh@gmail.com>
2023-04-19 15:03:26 +0800

    Update 2023-04-19T07:03:24.893Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,13 @@
 "use strict";
 
-console.log('Hello!');
+console.log("Hello!");
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
+  console.log("HesSchool Hello!");
+}); // toggle mobile nav menu
+
+var navBtn = document.querySelector("#navMobileBtn");
+var navMobileMenu = document.querySelector("#navMobileMenu");
+navBtn.addEventListener("click", function () {
+  navMobileMenu.classList.toggle("hidden");
 });
 //# sourceMappingURL=all.js.map
