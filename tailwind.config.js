b06b89ace4d24ef524822ea1075e22334f96f7e8
yachuh <yachuh@gmail.com>
2023-04-19 10:30:39 +0800

    Config font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,25 @@ module.exports = {
       center: true,
       padding: ".75rem",
     },
+    fontSize: {
+      xs: ["12px", "14px"],
+      sm: ["14px", "20px"],
+      base: ["16px", "24px"],
+      md: ["18px", "26px"],
+      lg: ["20px", "29px"],
+      xl: ["22px", "32px"],
+      "2xl": ["24px", "35px"],
+      "3xl": ["28px", "40px"],
+      "4xl": ["32px", "37px"],
+      "5xl": ["48px", "70px"],
+    },
     extend: {
       fontFamily: {
         azeret: ['"Azeret Mono"'],
       },
       colors: {
         transparent: "transparent",
-        qreen: "#62DB54", // 綠色
+        primary: "#62DB54", // 綠色
         black: {
           primary: "#1C1C1C", // 黑色, 主要背景
           secondary: "#2D2D2D", //深灰色, 次要背景
