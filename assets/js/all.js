4fe3b65152599b5ae10c13044f916b7b5519536c
yachuh <yachuh@gmail.com>
2023-04-27 12:31:37 +0800

    Update 2023-04-27T04:31:36.248Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -9,5 +9,32 @@ var navBtn = document.querySelector("#navMobileBtn");
 var navMobileMenu = document.querySelector("#navMobileMenu");
 navBtn.addEventListener("click", function () {
   navMobileMenu.classList.toggle("hidden");
+}); //=== Swiper ====//
+// // import Swiper bundle with all modules installed
+// import Swiper from "swiper-bundle";
+// // import styles bundle
+// import "swiper/css/bundle";
+// Initialize swiper
+
+var swiper = new Swiper(".course-swiper", {
+  autoplay: {
+    disableOnInteraction: false,
+    delay: 1000
+  },
+  speed: 1500,
+  loop: true,
+  slidesPerView: "auto",
+  //多欄
+  breakpoints: {
+    360: {
+      spaceBetween: 8
+    },
+    768: {
+      spaceBetween: 16
+    },
+    992: {
+      spaceBetween: 16
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
